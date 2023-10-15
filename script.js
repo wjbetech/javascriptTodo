6e51e19f345678bfda45e07dac68eca74339a526
wjbetech <wjbetech@gmail.com>
2023-10-15 15:40:21 +0900

    Finished vanilla JS Todo List

--- script.js
+++ script.js
@@ -3,36 +3,52 @@ const todoItems = [
   {task: "Learn JavaScript"},
   {task: "Learn React"},
   {task: "Give Maengsly a kiss"}
-]
+];
 
 
 // doc grabbers
-const searchInput = document.querySelector("#searchTodos");
-const newTodo = document.querySelector("#addTodo");
-const todosList = document.querySelector("#todos-list");
 const searchForm = document.querySelector("#search");
+const searchInput = document.querySelector("#searchTodos");
+const todosList = document.querySelector("#todos-list");
+const ul = document.querySelector("ul");
+const list = document.getElementsByClassName("todo-item");
+const newTodo = document.querySelector("#addTodo");
 const addForm = document.querySelector("#add");
-const ul = document.querySelector("ul")
-
-// passing data
-
-
 
 // events & functionality
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("Search submitted!")
-  const newSearch = searchForm.searchTodos.value;
 
-})
+// filter items to show
+const filterTodos = (term) => {
+  Array.from(ul.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("hide"))
 
+  Array.from(ul.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("hide"))
+};
+
+// search for an item
+searchForm.addEventListener("keyup", () => {
+  const term = searchInput.value.trim().toLowerCase();
+  filterTodos(term);
+});
+
+// delete an item
+todosList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("bx-trash-alt")) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
+// sanitize and add a new item
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("Form submitted!");
   const newTodo = addForm.addTodo.value.trim().charAt(0).toUpperCase() + addForm.addTodo.value.slice(1);
   console.log(`New Item: "${newTodo}"`);
   generateTemplateLi(newTodo);
-})
+});
 
 const generateTemplateLi = (todo) => {
   if (!todo.length) {
@@ -42,4 +58,4 @@ const generateTemplateLi = (todo) => {
     ul.innerHTML += html;
   }
   addForm.reset();
-}
+};
